87e4b3cfa79e48e0a110ae43be7420601dc1fd9f
makerGeek <moetez.93@gmail.com>
2016-04-30 13:21:41 +0100

    added moving algorithm to ballmover.js

--- starter/js/ballmover.js
+++ starter/js/ballmover.js
@@ -8,13 +8,27 @@ myTimer = setInterval(myFunction, 30);
 
 
 j= 0;
+var x=10;
+var y=10;
 function myFunction() {
 
   var a=$(".ball").offset().top;
   var b=$(".ball").offset().left;
-  $(".ball").offset({top: a + 10, left: b + 10});
+  $(".ball").offset({top: a + y, left: b + x});
+  if(a>$(".box").offset().top-70) {
+    y=-10;
+  }
+  if(a<70) {
+    y=10;
+  }
+  if(b>500) {
+    x=-10;
+  }
+  if(b<20) {
+    x=10;
+  }
     j++;
-    if (j > 20) {
+    if (j > 4000) {
         clearInterval(myTimer);
     }
 
