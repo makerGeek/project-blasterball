6c3f46ec0d5c00cd395ee7c54618ca7c24d6ad05
makerGeek <moetez.93@gmail.com>
2016-05-05 16:11:08 +0100

    ball movement is now OK

--- starter/js/ballmover.js
+++ starter/js/ballmover.js
@@ -1,13 +1,16 @@
-$(document).ready(function(){setTimeout(
-  function()
-  {
-myTimer = setInterval(myFunction, 30);
-   }, 2000);
-  }
+$(document).ready(function() {
+    setTimeout(
+    function() 
+    {
+        myTimer = setInterval(myFunction, 30);
+        console.log("started ballmover");
+    }, 2000);
+}
 )
 
 
-j= 0;
+j = 0;
+
 var xSpeed=10;
 var ySpeed=10;
 var offset = $(".playground").offset();
@@ -16,23 +19,52 @@ var leftLimit = offset.left;
 var bottomLimit = topLimit+$(".playground").innerHeight();
 var rightLimit = leftLimit+$(".playground").innerWidth();
 
-function myFunction() {
 
-  var y=$(".ball").offset().top;
-  var x=$(".ball").offset().left;
-  $(".ball").offset({top: y + ySpeed, left: x + xSpeed});
-  if(y>$(".playground").offset().top+$(".playground").height()) {
-    ySpeed=-10;
-  }
-  if(y<topLimit) {
-    ySpeed=10;
-  }
-  if(x>rightLimit) {
-    xSpeed=-10;
-  }
-  if(x<leftLimit) {
-    xSpeed=10;
-  }
+/*
+var xSpeed = 10;
+var ySpeed = 10;
+var topLimit = 40;
+var leftLimit = 380;
+var bottomLimit = 550;
+var rightLimit = 900;
+*/
+
+function myFunction() {
+    console.log("ooooo");
+    
+    var y = $(".ball").offset().top;
+    var x = $(".ball").offset().left;
+    var box = $(".box");
+    $(".ball").offset({
+        top: y + ySpeed,
+        left: x + xSpeed
+    });
+
+  
+    if (y > $(".box").offset().top - 20 && y < $(".box").offset().top + 20) {
+        if (x > box.offset().left && x < box.offset().left + box.width())
+            ySpeed = -10;
+    
+    } 
+      /*
+    else 
+    if (y > bottomLimit)
+        clearInterval(myTimer);
+        */
+
+    
+    if(y+$(".ball").height() > bottomLimit){
+        ySpeed=-10;
+    }
+    if (y < topLimit) {
+        ySpeed = 10;
+    }
+    if (x+$(".ball").width() > rightLimit) {
+        xSpeed = -10;
+    }
+    if (x < leftLimit) {
+        xSpeed = 10;
+    }
     j++;
     if (j > 4000) {
         clearInterval(myTimer);
