f11283548b40f0b3d1b3123c432e9c3c26d21b98
makerGeek <moetez.93@gmail.com>
2016-05-05 11:57:00 +0100

    modified ball movement limits

--- starter/js/ballmover.js
+++ starter/js/ballmover.js
@@ -8,24 +8,30 @@ myTimer = setInterval(myFunction, 30);
 
 
 j= 0;
-var x=10;
-var y=10;
+var xSpeed=10;
+var ySpeed=10;
+var offset = $(".playground").offset();
+var topLimit = offset.top;
+var leftLimit = offset.left;
+var bottomLimit = topLimit+$(".playground").innerHeight();
+var rightLimit = leftLimit+$(".playground").innerWidth();
+
 function myFunction() {
 
-  var a=$(".ball").offset().top;
-  var b=$(".ball").offset().left;
-  $(".ball").offset({top: a + y, left: b + x});
-  if(a>$(".box").offset().top-70) {
-    y=-10;
+  var y=$(".ball").offset().top;
+  var x=$(".ball").offset().left;
+  $(".ball").offset({top: y + ySpeed, left: x + xSpeed});
+  if(y>$(".playground").offset().top+$(".playground").height()) {
+    ySpeed=-10;
   }
-  if(a<70) {
-    y=10;
+  if(y<topLimit) {
+    ySpeed=10;
   }
-  if(b>500) {
-    x=-10;
+  if(x>rightLimit) {
+    xSpeed=-10;
   }
-  if(b<20) {
-    x=10;
+  if(x<leftLimit) {
+    xSpeed=10;
   }
     j++;
     if (j > 4000) {
