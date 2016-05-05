90dbfabad79efa8e972f2858014506bf92856898
makerGeek <moetez.93@gmail.com>
2016-05-05 11:04:44 +0100

    made bricks of width 2 cols

--- starter/js/init.js
+++ starter/js/init.js
@@ -1,10 +1,10 @@
 
 i = 0;
 function myFunction() {
-    $(".playground").append("<div class=\"brick\"></div>");
+    $(".playground").append("<div class=\"brick col-sm-2 col-xs-2 col-md-2\" ></div>");
     i++;
     console.log("printing brick " + i);
-    if (i > 22) {
+    if (i > 23) {
         clearInterval(myTimer);
         console.log("cleared")
         $(".playground").append("<div class=\"box\"></div>");
