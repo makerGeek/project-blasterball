e356a50825b4f85283daac814ad45371f4f62c0c
makerGeek <moetez.93@gmail.com>
2016-05-11 22:05:49 +0100

    background gets colored when game is paused

--- starter/js/mover.js
+++ starter/js/mover.js
@@ -35,9 +35,11 @@ $(document).keyup(function(e) {
       if(gameOn==1){
            clearInterval(myTimer);
         gameOn=0;
+            $(".playground").css("background", "crimson");
       } else {
           gameOn=1;
           myTimer = setInterval(myFunction, 15);
+           $(".playground").css("background", "rgba(97, 221, 158, 0.63)");
       }
 
   }
